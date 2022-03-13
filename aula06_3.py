dad4700cb94e4e22a38b43be3e31e799f3af769a
Pericles Zapata de Oliveira <pzoliveira@outlook.com>
2022-03-13 17:46:37 -0300

    Aula 6 - part 3

---
+++ aula06_3.py
@@ -0,0 +1,4 @@
+seta = {100, 200, 300}
+setb = {100, 200, 300, 400, 500, 600, 700}
+print('Set a is subset of set b? {}'.format(seta.issubset(setb)))
+print('Set b is superset of set a? {}'.format(setb.issuperset(seta)))
