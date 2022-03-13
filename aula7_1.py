aaa226165453ae05c3e3770a430e1044e4a0489f
Pericles Zapata de Oliveira <pzoliveira@outlook.com>
2022-03-13 18:01:24 -0300

    Aula 7 - part 1

---
+++ aula7_1.py
@@ -0,0 +1,20 @@
+def sums(a, b):
+    return a + b
+
+
+def subtr(a, b):
+    return a - b
+
+
+def mult(a, b):
+    return a * b
+
+
+def div(a, b):
+    return a / b
+
+
+print(sums(1, 2))
+print(subtr(3, 1))
+print(mult(5, 4))
+print(div(100, 50))
