d19871bf2f0fdb59cb0bf83586cee80e1a2d5c2e
Pericles Zapata de Oliveira <pzoliveira@outlook.com>
2022-03-17 22:26:43 -0300

    Aula 9 - final

--- aula09_1.py
+++ aula09_1.py
@@ -1,18 +1,38 @@
-def crtfile(txtdata):
-    txtfile = open('txtfile.txt', 'w')
+import shutil
+
+def crtfile(flnm, txtdata):
+    txtfile = open(flnm, 'w')
     txtfile.write(txtdata)
-    txtfile.write('\n')
     txtfile.close()
 
 
-def updtfile(txtdata):
-    txtfile = open('txtfile.txt', 'a')
+def updtfile(flnm, txtdata):
+    txtfile = open(flnm, 'a')
     txtfile.write(txtdata)
-    txtfile.write('\n')
     txtfile.close()
+
+
+def grdsavrg(flnm):
+    grdsfile = open(flnm, 'r')
+    cntnt = grdsfile.read()
+    linelst = cntnt.split('\n')
+    stdgrd = {}
+    for line in linelst:
+        wrdlst = line.split(',')
+        stdntnm = wrdlst.pop(0)
+        avggrd = lambda grdlst: sum([int(grd) for grd in grdlst]) / 4
+        stdgrd[stdntnm] = avggrd(wrdlst)
+    return stdgrd
 
 
 if __name__ == '__main__':
-    crtfile('First line')
-    updtfile('Second line')
-    updtfile('Third line')
+    crtfile('D:/PycharmProjects/DIOPyIntro/txtfile.txt', 'First line\n')
+    updtfile('txtfile.txt', 'Second line\n')
+    updtfile('txtfile.txt', 'Third line')
+    crtfile('grdsfile.txt', 'Aaron,5,5,5,5\n')
+    updtfile('grdsfile.txt', 'Ben,6,6,6,6\n')
+    updtfile('grdsfile.txt', 'Charles,7,7,7,7\n')
+    updtfile('grdsfile.txt', 'David,8,8,8,8')
+    print(grdsavrg('grdsfile.txt'))
+    shutil.copy('txtfile.txt', 'D:/PycharmProjects/text_file.txt')
+    shutil.move('grdsfile.txt', 'D:/PycharmProjects/grades_file.txt')
